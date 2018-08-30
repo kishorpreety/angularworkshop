4025980a59a27f0917b8cc5f5d8a2aa8fb74c4cf
Purushottam Kumar <purushottam.kumar@onsumaye.com>
2018-08-30 12:25:48 +0530

    commit changes on git hub update code

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -6,10 +6,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent  {
- log(x){ console.log(x);}
+	
+	public backsignupshow:boolean = false;
+	public playersignupshow:boolean = false;
+	public teamsignupshow:boolean = false;
+	public companysignupshow:boolean = false;
+	public signuptypeshow:boolean = true;
+// log(x){ console.log(x);}
 
- submit(f){
-   f.value
+// submit(f){
+ //  f.value
   // console.log(f);
- }
+// }
+
+	showPlayerSignupForm(){
+		 console.log('Purushottam Kumar');
+		 this.signuptypeshow = false;
+		 this.teamsignupshow = false;
+		 this.companysignupshow = false;
+		 this.playersignupshow = true;
+		 this.backsignupshow = true;
+	}
+	 showTeamSignupForm(){
+		 console.log('Purushottam Kumar');
+		 this.signuptypeshow = false;
+		 this.playersignupshow = false;
+		 this.companysignupshow = false;
+		 this.teamsignupshow = true;
+		 this.backsignupshow = true;
+	}
+	showCompanySignupForm(){
+		 console.log('Purushottam Kumar');
+		 this.signuptypeshow = false;
+		 this.playersignupshow = false;
+		 this.companysignupshow = true;
+		 this.teamsignupshow = false;
+		 this.backsignupshow = true;
+	}
+	showSignupTypes(){
+		 console.log('Purushottam Kumar');
+		 this.playersignupshow = false;
+		 this.companysignupshow = false;
+		 this.teamsignupshow = false;
+		 this.backsignupshow = false;
+		 this.signuptypeshow = true;
+		 
+	}
 }
--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -26,6 +26,7 @@ $(function() {
                 }, 800);
             }
         });
+       /*register page*/ 
       $(".register-choice-item").click(function() { 
         if($(this).hasClass("selected") == false) {
             $(".register-choice-item").removeClass("selected");
@@ -59,10 +60,54 @@ $(function() {
         $('.register-choice-item').delay(700).slideDown();
         $('.back').delay(800).fadeOut();
     });
+//end register page
+/*dashboard-player page*/
         $(".edit-profie").click(function(){
+             var docHeight = $(document).height();
+            $("body").append("<div id='overlay'></div>");
             $(".edit-player-profile").animate({
                 width: "toggle"
             });
+             $('.close').on("click", function () {
+            $('#overlay,.edit-player-profile').fadeOut('3000',function(){//use 3000 in place of 300m
+                $('#overlay').remove();
+            });    
+            return false;
+            }); 
+               $("#overlay")
+                .height(docHeight)
+                .css({
+                    'opacity' : 0.7,
+                    'position': 'absolute',
+                    'top': 0,
+                    'left': 0,
+                    'background-color': 'white',
+                    'width': '100%',
+                    'z-index': 5000
+                });
+          
         });
 
+      var divHeight = $('body').height(); 
+      $('.edit-player-profile').css('min-height', divHeight+'px');
+
+   /*Player profile page*/
+        $(".add-btn ").click(function() {
+                $('.remove-btn').show();
+            $('.add-btn').hide();
+            });
+            $(".remove-btn ").click(function() {
+                $('.add-btn').show();
+            $('.remove-btn').hide();
+            });
     });
+    document.getElementById("my-file").onchange = function() {
+    if (this.files && this.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            // e.target.result is a base64-encoded url that contains the image data
+        document.getElementById('profile-pic').setAttribute('src', e.target.result);
+        };
+        reader.readAsDataURL(this.files[0]);
+    }
+    }
